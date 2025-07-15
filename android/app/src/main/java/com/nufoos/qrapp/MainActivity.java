4da2bd664b1e5d9b6b0cde78b53cd984ceca3c4d
Nufoos-007 <nufoospm@gmail.com>
2025-07-16 01:10:03 +0530

    Initial commit - QR Code Generator with Android support

---
+++ android/app/src/main/java/com/nufoos/qrapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nufoos.qrapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nufoos.qrapp',
+  appName: 'React QR App',
+  webDir: 'dist'
+};
+
+export default config;
